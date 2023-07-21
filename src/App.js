c7fdd899e222605e761fe7bc3059e77ebe7d2874
Dharvo <adedave77@gmail.com>
2023-07-21 19:52:04 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/Components/Header.jsx 	modified:   src/Components/Navbar.jsx 	new file:   src/Styles/App.css 	new file:   src/Styles/App.scss 	new file:   src/Styles/Nav.scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import "./Styles/App.css";
+
 function App() {
   return (
     <div className="App bg-slate-100 w-full">
+      <Navbar />
+      <Header />
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React from "react";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className="Navbar flex bg-white shadow-neutral-100 px-8 py-2
+    items-center
+    justify-between"
+    >
+        <span>
 
-export default Navbar
+      <h1 className="font-sans font-extrabold text-2xl capitalize">
+        wha
+        <span className="text-zinc-800">
+        todo
+        </span>
+        <span className="text-zinc-700 font-normal text-sm respect">&reg;</span>
+      </h1>
+        </span>
+      <div className="panel">
+        <div className="user font-sans ">
+          <p className="text-zinc-800 font-bold leading-tight ">John Doe</p>
+          <p className="text-blue-600 font-semibold text-sm leading-tight">(User)</p>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
