480c3781ce019ea1356c34b9d416db6e6458cfe3
Dharvo <adedave77@gmail.com>
2023-07-21 21:39:50 +0100

    modified:   src/Components/Header.jsx 	modified:   src/Components/Navbar.jsx 	new file:   src/Icons/AddUser.jsx 	new file:   src/Icons/Bell.jsx 	new file:   src/Icons/BellAlert.jsx 	new file:   src/Icons/BellSlash.jsx 	new file:   src/Icons/BellSnooze.jsx 	new file:   src/Icons/Chart.jsx 	new file:   src/Icons/ChartBar.jsx 	new file:   src/Icons/ChevDown.jsx 	new file:   src/Icons/ChevDown2.jsx 	new file:   src/Icons/ChevUpDown.jsx 	new file:   src/Icons/Funnel.jsx 	new file:   src/Icons/Infocircle.jsx 	new file:   src/Icons/Option2.jsx 	new file:   src/Icons/Plus.jsx 	new file:   src/Icons/PlusCircle.jsx 	new file:   src/Icons/Settings6.jsx 	new file:   src/Icons/Settings8.jsx 	renamed:    Dashboard - Admin.png -> src/Images/Dashboard - Admin.png 	renamed:    Dashboard - User.png -> src/Images/Dashboard - User.png 	new file:   src/Images/a40e4d1afec5e099a8d106cac45ac9ef.jpg 	new file:   src/Images/e3c0b57e39b9038b5a33a28d7953f24d.jpg 	new file:   src/Images/photo-1557053910-d9eadeed1c58.jfif 	modified:   src/Styles/App.css 	modified:   src/Styles/Nav.scss

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Plus from "../Icons/Plus";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="container">
+      <div className="py-2 px-6 flex justify-between items-center">
+        <button className="flex items-center justify-between bg-transparent rounded-sm shadow-lg shadow-neutral-200 px-3.5 py-1 pt-1.5 uppercase text-slate-300 transition-all ease-in btn-text">
+          <span className="[&>svg]:w-4 pr-2 ">
+            <Plus />
+          </span>
+          create todo
+        </button>
+        <p className="text-blue-700 text-bold font-semibold tracking-tight">
+          Dashboard
+        </p>
+      </div>
+
+      <div className="py-2 px-6 flex items-right justify-evenly w-60 ml-auto mr--3">
+        {/* <button className="bg-transparent rounded-sm shadow-lg shadow-neutral-200 px-3.5 py-1.5 uppercase text-slate-300 transition-all ease-in btn-text">
+  create todo
+</button> */}
+        <span></span>
+        <p className="text-gray-700 font-sans font-bold text-small uppercase tracking-tight">
+          all
+        </p>
+        <p className="text-gray-300 font-sans font-bold text-small capitalize tracking-tight">
+          filter
+        </p>
+        <p className="text-gray-500 font-sans font-bold text-small uppercase ">
+          sort
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,47 @@
 import React from "react";
+import Bell from "../Icons/Bell";
+import Setting6 from "../Icons/Settings6";
+import img from "../Images/a40e4d1afec5e099a8d106cac45ac9ef.jpg";
+import pic from "../Images/e3c0b57e39b9038b5a33a28d7953f24d.jpg";
 const Navbar = () => {
   return (
     <nav
       className="Navbar flex bg-white shadow-neutral-100 px-8 py-2
-    items-center
-    justify-between"
+      items-center
+      justify-between"
     >
-        <span>
-
-      <h1 className="font-sans font-extrabold text-2xl capitalize">
-        wha
-        <span className="text-zinc-800">
-        todo
-        </span>
-        <span className="text-zinc-700 font-normal text-sm respect">&reg;</span>
-      </h1>
-        </span>
-      <div className="panel">
+      {/* LEFT SIDE */}
+      <span>
+        <h1 className="font-sans font-extrabold text-2xl capitalize text-zinc-800">
+          wha
+          <span className="text-zinc-700">todo</span>
+          <span className="text-stone-500 font-normal text-sm respect">
+            &reg;
+          </span>
+        </h1>
+      </span>
+      {/* RIGHT SIDE */}
+      <div className="panel flex">
         <div className="user font-sans ">
           <p className="text-zinc-800 font-bold leading-tight ">John Doe</p>
-          <p className="text-blue-600 font-semibold text-sm leading-tight">(User)</p>
+          <p className="text-blue-600 font-semibold text-sm leading-tight">
+            (User)
+          </p>
         </div>
+        <img
+          class="w-8 mx-2.5 my-auto h-8 rounded-full shadow shadow-lg"
+          src={pic}
+          alt="Rounded avatar"
+        />
+        <span className="nav-icons p-1">
+        {/* <span className="m-auto [&>svg]:w-5.5 [&>svg]:text-blue-400 w-fit relative"> */}
+          <Bell />
+          {/* <span class="top-0 left-3 absolute w-3 h-3 bg-blue-500 border-2 border-white dark:border-gray-800 rounded-full"></span> */}
+        </span>
+        <span className="nav-icons pl-2">
+          <Setting6 />
+        </span>
+        <span className="[&>svg]:w-5"></span>
       </div>
     </nav>
   );
